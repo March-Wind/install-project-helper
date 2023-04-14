42b40cb8280345fcb5f6c7966507d0d9831e6a9f
liuzhiyang <zhiyang1.liu@ximalaya.com>
2023-04-14 19:23:59 +0800

    feat: cjs

--- src/task/styleint.ts
+++ src/task/styleint.ts
@@ -10,14 +10,14 @@ const tasks = new Listr<Ctx>([
   {
     title: 'install packages',
     task: () => {
-      return execa('npm', ['install', '-D', 'stylelint-config-prettier', 'stylelint-prettier'])
+      return execa('npm', ['install', '-D', 'stylelint@14.3.0','stylelint-config-prettier@9.0.3', 'stylelint-config-standard@24.0.0','stylelint-prettier@2.0.0'])
     }
   },
   {
     title: 'copy ..stylelintrc file',
     task: () => {
       const prefix = global.CONFIG_FILE_PATH;
-      return execa('cp', [path.resolve(prefix, '.stylelintrc.js'), process.cwd()])
+      return execa('cp', [path.resolve(prefix, '.stylelintrc.cjs'), process.cwd()])
     }
   },
 ]);
